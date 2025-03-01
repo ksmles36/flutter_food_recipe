6c36127b15db57afe57f33c54a5c7041efc818e4
kimseongmin <ksmles36@gmail.com>
2025-03-01 18:35:02 +0900

    실습 - food recipe - 화면 그리기 실습 완성코드 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePage(), // 홈페이지 보여주기
     );
   }
 }
@@ -21,59 +21,248 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 음식 사진 데이터
     List<Map<String, dynamic>> dataList = [
       {
         "category": "수제버거",
-        "imgUrl":
-            "https://i.ibb.co/HBGKYn4/foodiesfeed-com-summer-juicy-beef-burger.jpg",
+        "imgUrl": "https://devclass.devstory.co.kr/flutter-basic/1/food-burger.jpg",
       },
       {
         "category": "건강식",
-        "imgUrl":
-            "https://i.ibb.co/mB5YNs2/foodiesfeed-com-pumpkin-soup-with-pumpkin-seeds-on-top.jpg",
+        "imgUrl": "https://devclass.devstory.co.kr/flutter-basic/1/food-soup.jpg",
       },
       {
         "category": "한식",
-        "imgUrl":
-            "https://i.ibb.co/Kzzpc97/Beautiful-vibrant-shot-of-traiditonal-Korean-meals.jpg",
+        "imgUrl": "https://devclass.devstory.co.kr/flutter-basic/1/food-korean-meals.jpg",
       },
       {
         "category": "디저트",
-        "imgUrl":
-            "https://i.ibb.co/DL5vJVZ/foodiesfeed-com-carefully-putting-a-blackberry-on-tiramisu.jpg",
+        "imgUrl": "https://devclass.devstory.co.kr/flutter-basic/1/food-tiramisu.jpg",
       },
       {
         "category": "피자",
-        "imgUrl": "https://i.ibb.co/qsm8QH4/pizza.jpg",
+        "imgUrl": "https://devclass.devstory.co.kr/flutter-basic/1/food-pizza.jpg",
       },
       {
         "category": "볶음밥",
-        "imgUrl":
-            "https://i.ibb.co/yQDkq2X/foodiesfeed-com-hot-shakshuka-in-a-cast-iron-pan.jpg",
+        "imgUrl": "https://devclass.devstory.co.kr/flutter-basic/1/food-pizza.jpg",
       },
     ];
 
+    // 화면에 보이는 영역
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.black,
-        centerTitle: false,
-        iconTheme: IconThemeData(color: Colors.white),
+        elevation: 0, // 그림자 없애기
+        backgroundColor: Colors.white, // 배경 색상
+        centerTitle: false, // title 중앙 정렬
+        iconTheme: IconThemeData(color: Colors.black), // app bar icon color
         title: Text(
-          "food recipe",
+          "Food Recipe",
           style: TextStyle(
-              color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
+            color: Colors.black,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         actions: [
+          /// My Button
           IconButton(
             onPressed: () {
-              print("hello~~ ksm hi~");
+              print("go my page");
             },
-            icon: Icon(Icons.person_outline_rounded),
-            color: Colors.yellow,
-            tooltip: "안녕 나는 성미니야",
-          )
+            icon: Icon(
+              Icons.person_outline,
+            ),
+          ),
         ],
+      ),
+      body: Column(
+        children: [
+          /// 검색
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "상품을 검색해주세요.",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                // 돋보기 아이콘
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    print("돋보기 아이콘 클릭");
+                  },
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 1),
+
+          /// 카테고리 목록
+          Expanded(
+            child: ListView.builder(
+              // 보여주려는 데이터 개수
+              itemCount: dataList.length,
+              // itemCount 만큼 반복되며 화면에 보여주려는 위젯
+              // index가 0부터 dataList.length - 1까지 증가하며 전달됩니다.
+              itemBuilder: (context, index) {
+                // dataList에서 index에 해당하는 data 꺼내기
+                Map<String, dynamic> data = dataList[index];
+                String category = data["category"];
+                String imgUrl = data["imgUrl"];
+
+                // 카드 형태의 위젯
+                return Card(
+                  margin: const EdgeInsets.all(8),
+                  // 위젯들을 위로 중첩하기 위해 Stack 위젯 사용
+                  child: Stack(
+                    alignment: Alignment.center, // 중앙 정렬
+                    children: [
+                      /// 배경 이미지
+                      Image.network(
+                        imgUrl,
+                        width: double.infinity, // 가득 채우기
+                        height: 120,
+                        fit: BoxFit.cover, // 이미지 비율을 유지하며 주어진 width와 height를 가득 채우기
+                      ),
+
+                      /// 배경 위에 글씨가 보이도록 반투명한 박스 추가
+                      Container(
+                        width: double.infinity,
+                        height: 120,
+                        color: Colors.black.withOpacity(0.5), // 투명도 50%
+                      ),
+
+                      /// 카테고리
+                      Text(
+                        category,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+
+      /// AppBar 왼쪽에 아이콘을 누를 때 왼쪽에서 오른쪽으로 나오는 위젯
+      /// 해당 위젯을 추가하면 AppBar 왼쪽에 아이콘이 자동으로 추가됨
+      drawer: Drawer(
+        child: Column(
+          children: [
+            // Drawer 윗 부분
+            DrawerHeader(
+              margin: const EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: SizedBox(
+                width: double.infinity, // 가득 채우기
+                child: Column(
+                  children: [
+                    /// 동그란 배경
+                    CircleAvatar(
+                      radius: 36,
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+
+                        /// 이미지
+                        child: Image.network(
+                          "https://devclass.devstory.co.kr/flutter-basic/1/cat.png",
+                          width: 62,
+                        ),
+                      ),
+                    ),
+
+                    /// 닉네임 상단 여백
+                    SizedBox(height: 13),
+
+                    /// 닉네임
+                    Text(
+                      "닉네임",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    /// 이메일
+                    Text(
+                      "hello@world.com",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            /// 이벤트 배너
+            // 특정 비율로 위젯을 보여주기
+            AspectRatio(
+              aspectRatio: 12 / 4, // 가로 : 세로 = 12 : 4 (이미지를 12:4로 준비했어요)
+              /// 이벤트 배너 리스트
+              /// 스크롤을 하는데, 특정 항목이 스냅이 걸리도록 만들고 싶은 경우 PageView를 사용해요.
+              child: PageView(
+                children: [
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner1.jpg",
+                  ),
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner2.jpg",
+                  ),
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner3.jpg",
+                  ),
+                  Image.network(
+                    "https://devclass.devstory.co.kr/flutter-basic/1/sale-event-banner4.jpg",
+                  ),
+                ],
+              ),
+            ),
+
+            /// 구매 내역
+            ListTile(
+              title: Text(
+                '구매내역',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
+            ),
+
+            /// 레시피 공유하기
+            ListTile(
+              title: Text(
+                '저장한 레시피',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
